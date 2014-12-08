cdb3d43f2367f7bd0a639023024d1e0f3c2e2937
btssio <btssio@btssio-VirtualBox>
2014-12-08 15:22:38 +0100

    add style

--- vues/templates/template.inc.php
+++ vues/templates/template.inc.php
@@ -1,26 +1,45 @@
-﻿<!DOCTYPE html >
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html >
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../vues/css/styleLargeurFixe.css" />
-        <script type="text/javascript" src="../vues/javascript/fonctionsJavascript.inc.js"></script>
-        <title><?php echo $this->lireDonnee('titreVue'); ?></title>
+        <link rel="stylesheet" href="../css/stylesheet.css" />
+        <title></title>
     </head>
     <body>
-        <div id="global">
-            <header>
-               <?php include($this->lireDonnee('entete')); ?>
-            </header>
-            <nav>
-               <?php include($this->lireDonnee('gauche')); ?>
-            </nav>
-            <section>
-                <?php include($this->lireDonnee('centre'));?>
-            </section>
-            <footer>
-                <?php include($this->lireDonnee('pied'));?>
-            </footer>
+        <div id="header">
+            <div id="margo">
+                Margo
+            </div>
+            <div id="headerleft">?php include($this->lire('entete')); ?></div>
+            <div id="headerRight">
+                <a href="">Mon compte</a>
+                |
+                <a href="">Deconnexion</a>
+            </div>
+        </div>
+        <section>
+            <div class="left">
+                <h4>Menu principal</h4>
+                <ul>
+                    <li><a href="">Acceuil</a></li>
+                    <li><a href="">Enseignements</a></li>
+                    <li><a href="">Fillières</a></li>
+                    <li><a href="">Classes</a></li>
+                    <li><a href="">Elèves</a></li>
+                </ul>
+            </div>
+        </section>
+        <div class="right">
+            ?php include($this->lire('centre')); ?>
         </div>
     </body>
+    <footer>
+        ?php include($this->lire('pied')); ?>
+    </footer>
+</html>    
+</body>
 </html>
-
