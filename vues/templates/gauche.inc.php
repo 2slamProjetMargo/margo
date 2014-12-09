3e5d74f1a95b1a70ea242cfae8c258c47d079ace
btssio <btssio@btssio-VirtualBox>
2014-12-09 11:49:51 +0100

    ajout classes metiers

--- vues/templates/gauche.inc.php
+++ vues/templates/gauche.inc.php
@@ -7,6 +7,6 @@
         <li><a href="">Enseignements</a></li>
         <li><a href="">Fillières</a></li>
         <li><a href="">Classes</a></li>
-        <li><a href=".?controleur=Eleve&action=afficheListeEleve">Elèves</a></li>
+        <li><a href=".?controleur=Etudiant&action=afficheListeEtudiants">Elèves</a></li>
     </ul>
 </fieldset>
